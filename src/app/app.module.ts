07351863c321daeee582bae4ba27b5396a4c1eb6
Saranya <sarakrish34@gmail.com>
2023-10-08 16:49:48 -0500

    Updating angular version 16 to have the components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,20 @@
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
+import{TranslateHttpLoader} from '@ngx-translate/http-loader';
+import{TranslateLoader,TranslateModule} from '@ngx-translate/core';
+
+export function HttpLoaderFactory(http: HttpClient){
+  let fullLocationPath = location.host + location.pathname;
+
+   return new TranslateHttpLoader(http);
+}
+
 
 @NgModule({
   declarations: [
@@ -10,7 +22,16 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+  
+    // AppRoutingModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory:HttpLoaderFactory,
+        deps :[ HttpClient ],
+      },
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
